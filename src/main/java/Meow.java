d7cbb172e9e882ee1b35c4b9c65d9d2a952cc4b5
haoknowah <nrgman@tds.net>
2024-06-26 17:01:26 -0500

    mac app

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -7,7 +7,6 @@ import calendarmaker.Objects.Window;
 public class Meow {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		Window frame = new Window("Calendar");
 		frame.pack();
 		frame.setLocationRelativeTo(null);
